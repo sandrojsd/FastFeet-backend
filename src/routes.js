f48ddd67173305a3ea0e2795f896e6eb1707a8eb
Alexsandro Trindade <sandrojsd@gmail.com>
2020-02-24 16:24:25 -0400

    Multer lib para controle de arquivos, configurado

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import SessionController from './app/controllers/SessionController';
 import RecipientsController from './app/controllers/RecipientsController';
+import FileController from './app/controllers/FileController';
 
 import authMiddlewares from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 
@@ -15,5 +19,7 @@ routes.get('/recipients', RecipientsController.index);
 routes.post('/recipients', RecipientsController.store);
 routes.put('/recipients/:id', RecipientsController.update);
 routes.delete('/recipients/:id', RecipientsController.delete);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
